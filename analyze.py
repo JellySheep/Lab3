b2077b6150889580f704dfb0d014765d0807f1a2
Artem <temik1121@gmail.com>
2026-05-26 14:34:32 +1000

    coms

--- analyze.py
+++ analyze.py
@@ -18,6 +18,6 @@ def get_stats(scan_file, bom_file):
 p1, vp1, v1 = get_stats("scan_before.json", "bom_before.cdx.json")
 p2, vp2, v2 = get_stats("scan_after.json", "bom_after.cdx.json")
 
-print("\n=== ДАННЫЕ ДЛЯ ТВОЕГО ОТЧЕТА ===")
+print("\n=== ДАННЫЕ ДЛЯ ОТЧЕТА ===")
 print(f"ДО ОБНОВЛЕНИЯ: Всего пакетов: {p1}, Уязвимых пакетов: {vp1}, Найдено уязвимостей: {v1}")
 print(f"ПОСЛЕ ОБНОВЛЕНИЯ: Всего пакетов: {p2}, Уязвимых пакетов: {vp2}, Найдено уязвимостей: {v2}")
